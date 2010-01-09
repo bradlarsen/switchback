ba4f55109e68e68e91b93578182f2b13b4d4d27c
Bradford Larsen <brad.larsen@gmail.com>
2010-01-09 16:25:29 -0500

    A warning about small int types.

--- TilesTypes.hpp
+++ TilesTypes.hpp
@@ -7,6 +7,10 @@
 #include <boost/integer_traits.hpp>
 
 
+// Beware!  Using small int types doesn't seem to play well with
+// overloading resolution.  When using the `<<' and `>>' operators,
+// the char version is selected.
+
 typedef boost::int_t<8>::least  Tile;
 typedef boost::uint_t<8>::least TileIndex;
 typedef boost::array<Tile, 16>  TileArray;
