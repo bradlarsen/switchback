cc0286a446eb78d906d1edb6e4160b45dc551e1d
Bradford Larsen <brad.larsen@gmail.com>
2010-01-13 00:51:11 -0500

    More switchback skeleton code.

--- src/search/switchback/Switchback.hpp
+++ src/search/switchback/Switchback.hpp
@@ -4,6 +4,7 @@
 
 #include <vector>
 
+#include <boost/array.hpp>
 #include <boost/none.hpp>
 #include <boost/optional.hpp>
 #include <boost/pool/pool_alloc.hpp>
@@ -20,23 +21,30 @@ template <
 class Switchback
 {
 private:
+  typedef typename Node::Cost Cost;
+  typedef typename Node::State State;
   typedef BucketPriorityQueue<Node> Open;
+  typedef boost::optional<typename Open::ItemPointer> MaybeItemPointer;
+
   typedef boost::unordered_map<
-    Node *,
-    boost::optional<typename Open::ItemPointer>,
-    PointerHash<Node>,
-    PointerEq<Node>
-    , boost::fast_pool_allocator<
-        std::pair<Node * const,
-                  boost::optional<typename Open::ItemPointer> > >
+    State *,
+    MaybeItemPointer,
+    PointerHash<State>,
+    PointerEq<State>
+    , boost::fast_pool_allocator< std::pair<State * const, MaybeItemPointer> >
     > Closed;
 
 private:
-  const Node * goal;
+  const Node *goal;
+  bool searched;
+
   const Domain &domain;
 
   unsigned num_expanded;
   unsigned num_generated;
+
+  boost::array<Open, Domain::num_abstraction_levels> opened;
+  boost::array<Closed, Domain::num_abstraction_levels> closed;
 
 private:
   Switchback(const Switchback<Domain, Node> &);
@@ -45,21 +53,25 @@ private:
 public:
   Switchback(const Domain &domain)
     : goal(NULL)
+    , searched(false)
     , domain(domain)
     , num_expanded(0)
     , num_generated(0)
   {
-    assert(false);
   }
 
   ~Switchback()
   {
-    assert(false);
   }
 
   void search()
   {
-    assert(false);
+    if (searched || goal != NULL)
+      return;
+    searched = true;
+
+    init_opened_and_closed();
+    goal = resume_search(0, &domain.get_goal_state());
   }
 
   const Node * get_goal() const
@@ -80,6 +92,25 @@ public:
   unsigned get_num_expanded() const
   {
     return num_expanded;
+  }
+
+
+private:
+  Cost heuristic(const unsigned level, const State *goal_state)
+  {
+    assert(false);
+    return 0;
+  }
+  
+  Node * resume_search(const unsigned level, const State *goal_state)
+  {
+    assert(false);
+    return NULL;
+  }
+
+  void init_opened_and_closed()
+  {
+    assert(false);
   }
 };
 
