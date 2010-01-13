467f3130a6b57c8773fcd6e6e14e2498b6bddb68
Bradford Larsen <brad.larsen@gmail.com>
2010-01-13 00:50:27 -0500

    typename cleanup.

--- src/search/astar/AStar.hpp
+++ src/search/astar/AStar.hpp
@@ -21,15 +21,18 @@ class AStar
 {
 private:
   typedef BucketPriorityQueue<Node> Open;
+  typedef boost::optional<typename Open::ItemPointer> MaybeItemPointer;
+
   typedef boost::unordered_map<
     Node *,
-    boost::optional<typename Open::ItemPointer>,
+    MaybeItemPointer,
     PointerHash<Node>,
     PointerEq<Node>
-    , boost::fast_pool_allocator<
-        std::pair<Node * const,
-                  boost::optional<typename Open::ItemPointer> > >
+    , boost::fast_pool_allocator< std::pair<Node * const, MaybeItemPointer > >
     > Closed;
+
+  typedef typename Closed::iterator ClosedIterator;
+  typedef typename Closed::const_iterator ClosedConstIterator;
 
 public:
   AStar(const Domain &domain)
@@ -56,16 +59,15 @@ public:
       return;
     searched = true;
 
-    typename std::vector<Node *> succs;    // re-use a stack-allocated
-                                           // vector for successor
-                                           // nodes, thus avoiding
-                                           // repeated heap
-                                           // allocation.
+    std::vector<Node *> succs;    // re-use a stack-allocated vector
+                                  // for successor nodes, thus
+                                  // avoiding repeated heap
+                                  // allocation.
 
     {
       assert(all_closed_item_ptrs_valid());
       Node *start_node = domain.create_start_node();
-      boost::optional<typename Open::ItemPointer> open_ptr = open.push(start_node);
+      MaybeItemPointer open_ptr = open.push(start_node);
       assert(open_ptr);
       closed[start_node] = open_ptr;
       assert(closed.find(start_node) != closed.end());
@@ -101,9 +103,9 @@ public:
         Node *succ = succs[succ_i];
         assert(open.size() <= closed.size());
         assert(all_closed_item_ptrs_valid());
-        typename Closed::iterator closed_it = closed.find(succ);
+        ClosedIterator closed_it = closed.find(succ);
         if (closed.find(succ) != closed.end()) {
-          boost::optional<typename Open::ItemPointer> &open_ptr = closed_it->second;
+          MaybeItemPointer &open_ptr = closed_it->second;
 
           if ( open_ptr ) {
             Node *old_succ = open.lookup(*open_ptr);
@@ -130,7 +132,7 @@ public:
         }
         else {
           // succ has not been generated before.
-          boost::optional<typename Open::ItemPointer> open_ptr = open.push(succ);
+          MaybeItemPointer open_ptr = open.push(succ);
           closed[succ] = open_ptr;
           assert(closed.find(open.lookup(*open_ptr)) != closed.end());
           assert(all_closed_item_ptrs_valid());
@@ -170,7 +172,7 @@ private:
     return true;
     std::cerr << "checking if item pointers are valid" << std::endl
               << "  " << closed.size() << " pointers to check" << std::endl;
-    for (typename Closed::const_iterator closed_it = closed.begin();
+    for (ClosedConstIterator closed_it = closed.begin();
          closed_it != closed.end();
          ++closed_it) {
       if ( closed_it->second && !open.valid_item_pointer(*closed_it->second) )
