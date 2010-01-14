4a3abee6bde97ccdf21b65c5b24ee33910870480
Bradford Larsen <brad.larsen@gmail.com>
2010-01-14 18:26:24 -0500

    Do not store blank index in TilesState15.
    Instead, calculate the blank index as-needed.  This eliminates a
    1-byte member from the class, but reduces the class's actual, padded
    size on 64-bit machines by 8 bytes, it seems.
    
    Memory usage is slightly down, and search time appears unaffected.

--- src/tiles/TilesState.hpp
+++ src/tiles/TilesState.hpp
@@ -14,17 +14,15 @@ class TilesState15
 {
 private:
   std::size_t hash_value;
-  TileIndex blank_index;
   TileArray tiles;
 
 
 public:
   TilesState15(const TileArray &tiles)
     : hash_value(compute_hash(tiles))
-    , blank_index(find_blank_index(tiles))
     , tiles(tiles)
   {
-    assert(tiles[blank_index] == 0);
+    assert(tiles[get_blank()] == 0);
   }
 
   inline Tile operator ()(unsigned i, unsigned j) const
@@ -38,7 +36,6 @@ public:
   {
     return
       hash_value == other.hash_value &&
-      blank_index == other.blank_index &&
       tiles == other.tiles;
   }
 
@@ -49,17 +46,17 @@ public:
 
   inline TileIndex get_blank() const
   {
-    return blank_index;
+    return find_blank_index(tiles);
   }
 
   inline TileIndex get_blank_row() const
   {
-    return blank_index / 4;
+    return get_blank() / 4;
   }
 
   inline TileIndex get_blank_col() const
   {
-    return blank_index % 4;
+    return get_blank() % 4;
   }
 
   inline const TileArray & get_tiles() const
@@ -103,9 +100,9 @@ public:
 private:
   inline TilesState15 move_blank(TileIndex new_blank_offset) const
   {
+    TileIndex blank_index = get_blank();
     TileIndex new_blank_index = blank_index + new_blank_offset;
     TilesState15 new_state(*this);
-    new_state.blank_index = new_blank_index;
     std::swap(new_state.tiles[blank_index], new_state.tiles[new_blank_index]);
     new_state.hash_value = compute_hash(new_state.tiles);
 
