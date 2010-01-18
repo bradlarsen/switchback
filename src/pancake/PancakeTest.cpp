2e201023f57af880b6ca16f352f6ee3d45cb49e1
Ethan Burns <burns.ethan@gmail.com>
2010-01-18 16:39:57 -0500

    Fix mindless errors.

--- src/pancake/PancakeTest.cpp
+++ src/pancake/PancakeTest.cpp
@@ -22,8 +22,8 @@ int main(void)
  	PancakeState14 s(cakes);
 
 	std::cout << s.get_hash_value() << ": " << s << std::endl;
-	std::cout << s s.get_hash_value() << ": " <<.flip(4) << std::endl;
-	std::cout << s s.get_hash_value() << ": " <<.flip(5) << std::endl;
+	std::cout << s.get_hash_value() << ": " << s.flip(4) << std::endl;
+	std::cout << s.get_hash_value() << ": " << s.flip(5) << std::endl;
 
 	return EXIT_SUCCESS;
 }
