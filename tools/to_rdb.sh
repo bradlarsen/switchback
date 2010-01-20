8f4bc94b2d4687c2855141b45f1284bdc3584529
Ethan Burns <burns.ethan@gmail.com>
2010-01-20 12:25:29 -0500

    Handle files where no solution was found.

--- tools/to_rdb.sh
+++ tools/to_rdb.sh
@@ -15,18 +15,6 @@ FILE=$(basename $FPATH)
 ALG=$(echo $FILE | awk -F_ '{print $1}')
 DOMAIN=$(echo $FILE | awk -F_ '{print $2}')
 NUM=$(echo $FILE | awk -F_ '{print $3}' | sed 's/.log//')
-COST=$(grep g: $FPATH | sed 's/.*: \(.*\).*/\1/')
-EXPANDED=$(grep expanded: $FPATH | sed 's/.*: \(.*\) (.*/\1/')
-GENERATED=$(grep generated: $FPATH | sed 's/.*: \(.*\) (.*/\1/')
-TIME=$(grep time: $FPATH | sed 's/.*: \(.*\) s.*/\1/')
-CLOSED_SIZE=$(grep "closed size:" $FPATH | sed 's/.*: \(.*\).*/\1/')
-CREATION_TIME=$(ls -l $FPATH | awk '{ print $6 " " $7 }')
-LOOKUPS=$(grep lookup $FPATH \
-    | sed 's/:/\t/' \
-    | sed 's/lookups, /\t/' \
-    | sed 's/hits (/\t/' \
-    | sed 's/)//' \
-    | sed 's/^[[:space:]]*/#altrow \"lookups\"\t/')
 
 DOMAIN_DIR="$ROOT_DIR/$DOMAIN"
 ALG_DIR="$DOMAIN_DIR/$ALG"
@@ -43,19 +31,49 @@ test -d $ALG_DIR || {
     touch $ALG_DIR/KEY=num
 }
 
-(
-echo -e "#start data file format 4"
-echo -e "#altcols \"lookups\"\t\"lookup lvl\"\t\"lookup count\"\t\"hit count\"\t\"hit percentage\""
-echo -e "$LOOKUPS"
-echo -e "#pair \"alg\"\t\"$ALG\""
-echo -e "#pair \"domain\"\t\"$DOMAIN\""
-echo -e "#pair \"num\"\t\"$NUM\""
-echo -e "#pair \"found solution\"\t\"yes\""
-echo -e "#pair \"final sol cost\"\t\"$COST\""
-echo -e "#pair \"total nodes expanded\"\t\"$EXPANDED\""
-echo -e "#pair \"total nodes generated\"\t\"$GENERATED\""
-echo -e "#pair \"total raw cpu time\"\t\"$TIME\""
-echo -e "#pair \"closed size\"\t\"$CLOSED_SIZE\""
-echo -e "#pair \"wall finish time\"\t\"$CREATION_TIME\""
-echo -e "#end data file format 4"
-) > $ALG_DIR/$NUM
+if grep -q "found a solution:" $FPATH;
+then
+    echo "$ALG, $NUM: Solution found"
+    COST=$(grep g: $FPATH | sed 's/.*: \(.*\).*/\1/')
+    EXPANDED=$(grep expanded: $FPATH | sed 's/.*: \(.*\) (.*/\1/')
+    GENERATED=$(grep generated: $FPATH | sed 's/.*: \(.*\) (.*/\1/')
+    TIME=$(grep time: $FPATH | sed 's/.*: \(.*\) s.*/\1/')
+    CLOSED_SIZE=$(grep "closed size:" $FPATH | sed 's/.*: \(.*\).*/\1/')
+    CREATION_TIME=$(ls -l $FPATH | awk '{ print $6 " " $7 }')
+    LOOKUPS=$(grep lookup $FPATH \
+	| sed 's/:/\t/' \
+	| sed 's/lookups, /\t/' \
+	| sed 's/hits (/\t/' \
+	| sed 's/)//' \
+	| sed 's/^[[:space:]]*/#altrow \"lookups\"\t/')
+
+    (echo -e "#start data file format 4"
+	echo -e "#altcols \"lookups\"\t\"lookup lvl\"\t\"lookup count\"\t\"hit count\"\t\"hit percentage\""
+	echo -e "$LOOKUPS"
+	echo -e "#pair \"alg\"\t\"$ALG\""
+	echo -e "#pair \"domain\"\t\"$DOMAIN\""
+	echo -e "#pair \"num\"\t\"$NUM\""
+	echo -e "#pair \"found solution\"\t\"yes\""
+	echo -e "#pair \"final sol cost\"\t\"$COST\""
+	echo -e "#pair \"total nodes expanded\"\t\"$EXPANDED\""
+	echo -e "#pair \"total nodes generated\"\t\"$GENERATED\""
+	echo -e "#pair \"total raw cpu time\"\t\"$TIME\""
+	echo -e "#pair \"closed size\"\t\"$CLOSED_SIZE\""
+	echo -e "#pair \"wall finish time\"\t\"$CREATION_TIME\""
+	echo -e "#end data file format 4") > $ALG_DIR/$NUM
+else				# no solution
+    echo "$ALG, $NUM: No solution found"
+    (echo -e "#start data file format 4"
+	echo -e "#pair \"alg\"\t\"$ALG\""
+	echo -e "#pair \"domain\"\t\"$DOMAIN\""
+	echo -e "#pair \"num\"\t\"$NUM\""
+	echo -e "#pair \"found solution\"\t\"no\""
+	echo -e "#pair \"final sol cost\"\t\"infinity\""
+	echo -e "#pair \"total nodes expanded\"\t\"infinity\""
+	echo -e "#pair \"total nodes generated\"\t\"infinity\""
+	echo -e "#pair \"total raw cpu time\"\t\"infinity\""
+	echo -e "#pair \"closed size\"\t\"infinity\""
+	echo -e "#pair \"wall finish time\"\t\"$CREATION_TIME\""
+	echo -e "#end data file format 4") > $ALG_DIR/$NUM
+
+fi
